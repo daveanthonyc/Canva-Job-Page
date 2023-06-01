dc575480c7d26112251d82428edb9e2ccacaa658
David Chiang <daveanthonyc@gmail.com>
2023-06-01 15:30:46 +1000

    fixed remaining bugs from folder structure change

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import "./assets/css/MainJobListingsView.css"
+import "./View/MainJobListingsView.css"
 import {
   createBrowserRouter,
   RouterProvider,
